1eb9c8fa930c089e40f551d6a4a5b2f9e8ec74d2
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-06 17:20:13 -0600

    Extract getAudioInputMediaDeviceMatch into a separate class and rework to not fetch current audio input media devices
    Current audio input media devices are to always be supplied to the function and it is now synchronous

--- src/utils/fetchMediaDevices.js
+++ src/utils/fetchMediaDevices.js
@@ -79,64 +79,6 @@ const fetchVideoOutputMediaDevices = async (isAggressive = true) => {
   return outputMediaDevices.filter(device => device.kind.includes("video"));
 };
 
-/**
- * Performs best-guess match of given audio input previousDeviceInfo against
- * the currently available audio input devices.
- *
- * A use case for this may be for determining a default audio device against an
- * Object which was populated from previously serialized data.
- *
- * @param {MediaDeviceInfo | Object} previousDeviceInfo A regular Object may be
- * passed if unable to acquire original MediaDeviceInfo (i.e. from a serialized
- * cache, etc.)
- * @param {boolean | Object} isAggressiveOrMockObject If boolean is given, it
- * will fetch the current audio input media devices; if an array is given, it
- * will use it for mock data.
- * @return {Promise<MediaDeviceInfo | Object | null>}
- */
-const fetchAudioInputMediaDeviceMatch = async (
-  previousDeviceInfo,
-  isAggressiveOrMockObject = true
-) => {
-  const inputMediaDevices =
-    typeof isAggressiveOrMockObject === "boolean"
-      ? await fetchAudioInputMediaDevices(isAggressive)
-      : isAggressiveOrMockObject;
-
-  if (previousDeviceInfo.deviceId) {
-    const matchedDevice = inputMediaDevices.find(
-      device => previousDeviceInfo.deviceId === device.deviceId
-    );
-
-    if (matchedDevice) {
-      return matchedDevice;
-    }
-  }
-
-  if (previousDeviceInfo.groupId) {
-    const matchedDevice = inputMediaDevices.find(
-      device => previousDeviceInfo.groupId === device.groupId
-    );
-
-    if (matchedDevice) {
-      return matchedDevice;
-    }
-  }
-
-  if (previousDeviceInfo.label) {
-    // Find first matched device based on label
-    const matchedDevice = inputMediaDevices.find(
-      device => previousDeviceInfo.label === device.label
-    );
-
-    if (matchedDevice) {
-      return matchedDevice;
-    }
-  }
-
-  return null;
-};
-
 module.exports = fetchMediaDevices;
 
 module.exports.fetchInputMediaDevices = fetchInputMediaDevices;
@@ -146,6 +88,3 @@ module.exports.fetchVideoInputMediaDevices = fetchVideoInputMediaDevices;
 module.exports.fetchOutputMediaDevices = fetchOutputMediaDevices;
 module.exports.fetchAudioOutputMediaDevices = fetchAudioOutputMediaDevices;
 module.exports.fetchVideoOutputMediaDevices = fetchVideoOutputMediaDevices;
-
-module.exports.fetchAudioInputMediaDeviceMatch =
-  fetchAudioInputMediaDeviceMatch;
