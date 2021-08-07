91febe9600431574d18de03a206021a62ba5e2c9
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-07 10:57:24 -0600

    Add prototype createTestVideoMediaStream

--- src/debug/index.js
+++ src/debug/index.js
@@ -1,5 +1,7 @@
 const createTestAudioMediaStream = require("./createTestAudioMediaStream");
+const createTestVideoMediaStream = require("./createTestVideoMediaStream");
 
 module.exports = {
   createTestAudioMediaStream,
+  createTestVideoMediaStream,
 };
