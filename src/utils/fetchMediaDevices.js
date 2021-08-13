1cb02e493889bd7fa183d25babcb4aa0a7d77c2a
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-13 10:48:28 -0600

    Implement cached diff of MediaDeviceInfo elements across subsequent calls to fetchMediaDevices

--- src/utils/fetchMediaDevices.js
+++ src/utils/fetchMediaDevices.js
@@ -1,34 +1,106 @@
 /**
  * Lists all input and output media devices.
  *
+ * IMPORTANT: Unlike the underlying call to
+ * navigator.mediaDevices.enumerateDevices, this function will resolve the same
+ * MediaDeviceInfo instances across subsequent calls (as long as isAggressive
+ * is not changed between calls).
+ *
  * @param {boolean} isAggressive? [optional; default=true]
  * @return {Promise<MediaDeviceInfo[]>}
  */
-const fetchMediaDevices = async (isAggressive = true) => {
-  if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-    console.warn("enumerateDevices() not supported.");
-    return [];
-  }
+const fetchMediaDevices = (() => {
+  const cache = {
+    lastIsAggressive: null,
+    lastMediaDevices: [],
+  };
 
-  const fetchDevices = () => navigator.mediaDevices.enumerateDevices();
+  return async (isAggressive = true) => {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+      console.warn("enumerateDevices() not supported.");
+      return [];
+    }
 
-  let devices = await fetchDevices();
+    const fetchDevices = () => navigator.mediaDevices.enumerateDevices();
 
-  // If not able to fetch label for all devices...
-  if (isAggressive && devices.some(({ label }) => !label.length)) {
-    // ... temporarily turn on microphone...
-    const tempMediaStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-    });
+    let devices = await fetchDevices();
 
-    //  ... and fetch again
-    devices = await fetchDevices();
+    // If not able to fetch label for all devices...
+    if (isAggressive && devices.some(({ label }) => !label.length)) {
+      // ... temporarily turn on microphone...
+      const tempMediaStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
 
-    // ... then turn off the mic
-    tempMediaStream.getTracks().forEach(track => track.stop());
-  }
+      //  ... and fetch again
+      devices = await fetchDevices();
 
-  return devices;
+      // ... then turn off the mic
+      tempMediaStream.getTracks().forEach(track => track.stop());
+    }
+
+    // If last run isAggressive is the same as the current, use the cacheDiff,
+    //otherwise bust the cache and start over
+    if (cache.lastIsAggressive === isAggressive) {
+      devices = cacheDiffMediaDevices(cache.lastMediaDevices, devices);
+    } else {
+      // Bust the cache and start over
+
+      // Set updated isAggressive
+      cache.lastIsAggressive = isAggressive;
+
+      // Reset device list
+      cache.lastMediaDevices = [];
+    }
+
+    cache.lastMediaDevices = [...devices];
+
+    return devices;
+  };
+})();
+
+/**
+ * @param {MediaDeviceInfo[] | Object[]} prevMediaDevices
+ * @param {MediaDeviceInfo[] | Object[]} nextMediaDevices
+ * @return {MediaDeviceInfo[] | Object[]}
+ */
+const cacheDiffMediaDevices = (prevMediaDevices, nextMediaDevices) => {
+  /**
+   * This will become what is written back to the mediaDevices state.
+   *
+   * This original value represents the current state of mediaDevices with
+   * removed new devices filtered out.
+   *
+   * @type {MediaDeviceList[]}
+   */
+  const next = prevMediaDevices.filter(device =>
+    Boolean(
+      nextMediaDevices.find(predicate => {
+        const isMatch =
+          predicate.kind === device.kind &&
+          predicate.deviceId === device.deviceId;
+
+        return isMatch;
+      })
+    )
+  );
+
+  // Add new media devices to the next array
+  nextMediaDevices.forEach(device => {
+    const isPrevious = Boolean(
+      prevMediaDevices.find(
+        predicate =>
+          predicate.kind === device.kind &&
+          predicate.deviceId === device.deviceId
+      )
+    );
+
+    if (!isPrevious) {
+      next.push(device);
+    }
+  });
+
+  return next;
 };
 
 /**
@@ -106,6 +178,8 @@ const filterVideoOutputDevices = mediaDeviceInfoList => {
 };
 
 module.exports = fetchMediaDevices;
+
+module.exports.cacheDiffMediaDevices = cacheDiffMediaDevices;
 
 module.exports.filterInputMediaDevices = filterInputMediaDevices;
 module.exports.filterAudioInputDevices = filterAudioInputDevices;
