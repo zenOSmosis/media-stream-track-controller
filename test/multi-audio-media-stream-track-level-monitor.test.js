f0a14a335714a42c668f284ec1395be36bf9c284
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-18 10:07:04 -0600

    Rename internal test variable

--- test/multi-audio-media-stream-track-level-monitor.test.js
+++ test/multi-audio-media-stream-track-level-monitor.test.js
@@ -13,14 +13,14 @@ test("MultiAudioMediaStreamTrackLevelMonitor MediaStreamTrack / LevelMonitor typ
   const testTrack3 = debug.createTestAudioMediaStream().getTracks()[0];
   const testTrack4 = debug.createTestAudioMediaStream().getTracks()[0];
 
-  const audioMonitor = new MultiAudioMediaStreamTrackLevelMonitor([
+  const multiAudioMonitor = new MultiAudioMediaStreamTrackLevelMonitor([
     testTrack1,
     testTrack2,
     testTrack3,
     testTrack4,
   ]);
 
-  audioMonitor.getChildren().forEach((child, idx) => {
+  multiAudioMonitor.getChildren().forEach((child, idx) => {
     t.ok(
       ![testTrack1, testTrack2, testTrack3, testTrack4].includes(child),
       "MediaStreamTrack array does not include direct children of collection"
@@ -32,7 +32,7 @@ test("MultiAudioMediaStreamTrackLevelMonitor MediaStreamTrack / LevelMonitor typ
     );
   });
 
-  audioMonitor.getMediaStreamTracks().forEach((track, idx) => {
+  multiAudioMonitor.getMediaStreamTracks().forEach((track, idx) => {
     t.ok([testTrack1, testTrack2, testTrack3, testTrack4].includes(track));
 
     t.ok(
@@ -44,7 +44,7 @@ test("MultiAudioMediaStreamTrackLevelMonitor MediaStreamTrack / LevelMonitor typ
   // FIXME: Using t.throws is not working correct w/ tape-async; this is a
   // workaround
   try {
-    await audioMonitor.removeChild("abc");
+    await multiAudioMonitor.removeChild("abc");
 
     throw new Error("should throw error");
   } catch (err) {
@@ -55,72 +55,74 @@ test("MultiAudioMediaStreamTrackLevelMonitor MediaStreamTrack / LevelMonitor typ
   }
 
   t.equals(
-    audioMonitor.getChildren().length,
-    audioMonitor.getMediaStreamTracks().length,
+    multiAudioMonitor.getChildren().length,
+    multiAudioMonitor.getMediaStreamTracks().length,
     "multi audio monitor contains same amount of tracks as children"
   );
 
   t.equals(
-    audioMonitor.getChildren().length,
+    multiAudioMonitor.getChildren().length,
     4,
     "multi audio monitor contains 4 tracks"
   );
 
-  await audioMonitor.removeMediaStreamTrack(testTrack3);
+  await multiAudioMonitor.removeMediaStreamTrack(testTrack3);
 
   t.equals(
-    audioMonitor.getChildren().length,
+    multiAudioMonitor.getChildren().length,
     3,
     "multi audio monitor contains 3 tracks"
   );
 
   t.equals(
-    audioMonitor.getChildren().length,
-    audioMonitor.getMediaStreamTracks().length,
+    multiAudioMonitor.getChildren().length,
+    multiAudioMonitor.getMediaStreamTracks().length,
     "multi audio monitor contains same amount of tracks as children"
   );
 
   t.throws(
     () => {
-      audioMonitor.addChild(new MediaStream(), "incorrect-type");
+      multiAudioMonitor.addChild(new MediaStream(), "incorrect-type");
     },
     TypeError,
     "throws TypeError when trying to add child of non-MediaStreamTrack type"
   );
 
   t.doesNotThrow(() => {
-    audioMonitor.addMediaStreamTrack(
+    multiAudioMonitor.addMediaStreamTrack(
       debug.createTestAudioMediaStream().getTracks()[0]
     );
   }, "accepts additional arbitrary call to addMediaStreamTrack()");
 
   t.doesNotThrow(() => {
-    audioMonitor.addChild(debug.createTestAudioMediaStream().getTracks()[0]);
+    multiAudioMonitor.addChild(
+      debug.createTestAudioMediaStream().getTracks()[0]
+    );
   }, "accepts additional arbitrary call to addChild() without a key");
 
   t.equals(
-    audioMonitor.getChildren().length,
+    multiAudioMonitor.getChildren().length,
     5,
     "multi audio monitor contains 5 tracks"
   );
 
-  await audioMonitor.removeAllMediaStreamTracks();
+  await multiAudioMonitor.removeAllMediaStreamTracks();
 
   t.equals(
-    audioMonitor.getChildren().length,
+    multiAudioMonitor.getChildren().length,
     0,
     "multi audio monitor contains 0 tracks after all are removed"
   );
 
   t.ok(
-    !audioMonitor.getIsDestroyed(),
+    !multiAudioMonitor.getIsDestroyed(),
     "multi audio monitor stays running after all tracks are removed"
   );
 
   t.end();
 });
 
-// TODO: For some reason when calling audioMonitor destroy, it locks up the
+// TODO: For some reason when calling multiAudioMonitor destroy, it locks up the
 // test runner (which is one reason the destruct test was isolated), though
 // none of the internal destruct handlers seem to lock it up on their own
 /*
@@ -132,14 +134,14 @@ test("MultiAudioMediaStreamTrackLevelMonitor shutdown handling", async t => {
   const testTrack3 = debug.createTestAudioMediaStream().getTracks()[0];
   const testTrack4 = debug.createTestAudioMediaStream().getTracks()[0];
 
-  const audioMonitor = new MultiAudioMediaStreamTrackLevelMonitor([
+  const multiAudioMonitor = new MultiAudioMediaStreamTrackLevelMonitor([
     testTrack1,
     testTrack2,
     testTrack3,
     testTrack4,
   ]);
 
-  await audioMonitor.destroy();
+  await multiAudioMonitor.destroy();
 
   t.notOk(
     t.getChildren().length,
