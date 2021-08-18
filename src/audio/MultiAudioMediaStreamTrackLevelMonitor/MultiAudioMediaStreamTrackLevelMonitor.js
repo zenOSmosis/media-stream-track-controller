0ae9764fdf9ad5db9bc45926c5cd3092ce5a2458
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-18 12:18:31 -0600

    Rename internal variable and add additional prototyping

--- src/audio/MultiAudioMediaStreamTrackLevelMonitor/MultiAudioMediaStreamTrackLevelMonitor.js
+++ src/audio/MultiAudioMediaStreamTrackLevelMonitor/MultiAudioMediaStreamTrackLevelMonitor.js
@@ -1,4 +1,6 @@
 const { PhantomCollection } = require("phantom-core");
+const { EVT_CHILD_INSTANCE_ADDED, EVT_CHILD_INSTANCE_REMOVED } =
+  PhantomCollection;
 const AudioMediaStreamTrackLevelMonitor = require("../AudioMediaStreamTrackLevelMonitor");
 const {
   /** @exports */
@@ -14,9 +16,15 @@ const {
 /** @exports */
 const EVT_DEBOUNCED_PEAK_AUDIO_LEVEL_TICK = `debounced-peak-${EVT_AUDIO_LEVEL_TICK}`;
 
-// TODO: Document
-// TODO: State that all tracks can be removed and the multi-monitor will stay running
 /**
+ * Listens to the audio levels of multiple audio tracks at once and emits a
+ * debounced peak audio level on each cycle of all of the children audio level
+ * listeners.
+ *
+ * Uses MediaStreamTracks themselves as PhantomCore instances by internally
+ * binding AudioMediaStreamTrackLevelMonitor instances to them when added as
+ * children.
+ *
  * IMPORTANT: This is intentionally designed so that all tracks can be removed
  * during runtime and the multi-monitor will stay running.
  */
@@ -34,20 +42,48 @@ class MultiAudioMediaStreamTrackLevelMonitor extends PhantomCollection {
     this._handleAudioLevelTick = this._handleAudioLevelTick.bind(this);
 
     this.on(EVT_AUDIO_LEVEL_TICK, this._handleAudioLevelTick);
+
+    // Used so we don't have to figure this out on every audio tick
+    this._lenChildren = this.getChildren().length;
+
+    (() => {
+      // Keep this._lenChildren in sync w/ number of actual children
+      const _handleChildrenUpdate = () =>
+        (this._lenChildren = this.getChildren().length);
+
+      this.on(EVT_CHILD_INSTANCE_ADDED, _handleChildrenUpdate);
+      this.on(EVT_CHILD_INSTANCE_REMOVED, _handleChildrenUpdate);
+    })();
+
+    // TODO: Document; both are used for debounced peak audio level
+    // determination
+    this._childTickIdx = -1;
+    this._currentPeakLevel = 0;
   }
 
   // TODO: Implement
   // Called when any child module emits an audio level
   _handleAudioLevelTick(audioLevel) {
+    ++this._childTickIdx;
+
     // TODO: Remove
     console.warn({
-      TODO: "Build out debounce peak audio level tick handling",
+      TODO: "Finish building out debounce peak audio level tick handling",
       audioLevel,
     });
 
-    // TODO: Get number of children and use % (modulus) to determine when to EVT_DEBOUNCED_PEAK_AUDIO_LEVEL_TICK
+    if (!((this._childTickIdx + 1) % this._lenChildren)) {
+      this.emit(EVT_DEBOUNCED_PEAK_AUDIO_LEVEL_TICK, this._currentPeakLevel);
 
-    // TODO: Integrate w/ EVT_DEBOUNCED_PEAK_AUDIO_LEVEL_TICK
+      // Reset
+      this._childTickIdx = -1;
+      this._currentPeakLevel = 0;
+    } else {
+      // TODO: Determine peak level from audio level and set it
+      // if (audioLevel > this._currentPeakLevel) {
+      // this._currentPeakLevel = audioLevel
+      // }
+    }
   }
 
   /**
@@ -62,7 +98,14 @@ class MultiAudioMediaStreamTrackLevelMonitor extends PhantomCollection {
     return this.destroy();
   }
 
-  // TODO: Document
+  /**
+   * Adds a MediaStreamTrack to the given collection.
+   *
+   * If a duplicate track is added, it will silently ignore the duplicate.
+   *
+   * @param {MediaStreamTrack} mediaStreamTrack
+   * @return {void}
+   */
   addChild(mediaStreamTrack) {
     if (!(mediaStreamTrack instanceof MediaStreamTrack)) {
       throw new TypeError("mediaStreamTrack is not a MediaStreamTrack");
@@ -80,7 +123,14 @@ class MultiAudioMediaStreamTrackLevelMonitor extends PhantomCollection {
     );
   }
 
-  // TODO: Document
+  /**
+   * Adds a MediaStreamTrack to the given collection.
+   *
+   * If a duplicate track is added, it will silently ignore the duplicate.
+   *
+   * @param {MediaStreamTrack} mediaStreamTrack
+   * @return {void}
+   */
   addMediaStreamTrack(mediaStreamTrack) {
     return this.addChild(mediaStreamTrack);
   }
@@ -95,31 +145,32 @@ class MultiAudioMediaStreamTrackLevelMonitor extends PhantomCollection {
     return this.destroyAllChildren();
   }
 
-  // TODO: Document
   /**
+   * Removes the given MediaStreamTrack or associated
+   * AudioMediaStreamTrackLevelMonitor from the underlying collection.
    *
-   * @param {MediaStreamTrack | AudioMediaStreamTrackLevelMonitor} child
-   * @return {Promise<void>} A promise is used because it needs to shut down the underlying track level monitor.
+   * IMPORTANT: This overrides the super's synchronous handling w/
+   * asynchronous (we're returning a promise here).
+   *
+   * @param {MediaStreamTrack | AudioMediaStreamTrackLevelMonitor} trackOrMonitor
+   * @return {Promise<void>} NOTE: A promise is used because it needs to shut
+   * down the associated track level monitor, which is an asynchronous call.
    */
-  async removeChild(child) {
+  async removeChild(trackOrMonitor) {
     if (
-      !(child instanceof MediaStreamTrack) &&
-      !(child instanceof AudioMediaStreamTrackLevelMonitor)
+      !(trackOrMonitor instanceof MediaStreamTrack) &&
+      !(trackOrMonitor instanceof AudioMediaStreamTrackLevelMonitor)
     ) {
       throw new TypeError(
-        "child must be either a MediaStreamTrack or AudioMediaStreamTrackLevelMonitor"
+        "trackOrMonitor must be either a MediaStreamTrack or AudioMediaStreamTrackLevelMonitor"
       );
     }
 
-    // TODO: Remove
-    if (typeof child === "string") {
-      throw "?";
-    }
-
+    /** @type {AudioMediaStreamTrackLevelMonitor} */
     const trackLevelMonitor =
-      child instanceof AudioMediaStreamTrackLevelMonitor
-        ? child
-        : this.getChildWithKey(child.id);
+      trackOrMonitor instanceof AudioMediaStreamTrackLevelMonitor
+        ? trackOrMonitor
+        : this.getChildWithKey(trackOrMonitor.id);
 
     if (trackLevelMonitor) {
       // NOTE: Destroying the track should also remove the child from super,
@@ -130,7 +181,12 @@ class MultiAudioMediaStreamTrackLevelMonitor extends PhantomCollection {
     }
   }
 
-  // TODO: Document
+  /**
+   *
+   * @param {MediaStreamTrack} mediaStreamTrack
+   * @return {Promise<void>} NOTE: A promise is used because it needs to shut
+   * down the associated track level monitor, which is an asynchronous call.
+   */
   async removeMediaStreamTrack(mediaStreamTrack) {
     return this.removeChild(mediaStreamTrack);
   }
