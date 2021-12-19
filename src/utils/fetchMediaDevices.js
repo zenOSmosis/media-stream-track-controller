3b27d1ac4ea9ff7765974d473ece680393d39b05
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-12-19 14:13:53 -0600

    Extract mediaDeviceInfoFilters into separate file

--- src/utils/fetchMediaDevices.js
+++ src/utils/fetchMediaDevices.js
@@ -1,3 +1,5 @@
+const { filterInputMediaDevices } = require("./mediaDeviceInfoFilters");
+
 /**
  * Lists all input and output media devices.
  *
@@ -134,104 +136,7 @@ const cacheDiffMediaDevices = (prevMediaDevices, nextMediaDevices) => {
   return next;
 };
 
-/**
- * Returns a filtered array of MediaDeviceInfo[-like] structures, representing
- * input devices only.
- *
- * NOTE: This was implemented as a separate array filter function in order to
- * perform unit testing.
- *
- * @param {MediaDeviceInfo[] | Object[]} mediaDeviceInfoList
- * @return {MediaDeviceInfo | Object}
- */
-const filterInputMediaDevices = mediaDeviceInfoList => {
-  return mediaDeviceInfoList.filter(device => device.kind.includes("input"));
-};
-
-/**
- * Returns a filtered array of MediaDeviceInfo[-like] structures, representing
- * audio input devices only.
- *
- * @param {MediaDeviceInfo[] | Object[]} mediaDeviceInfoList
- * @return {MediaDeviceInfo | Object}
- */
-const filterAudioInputDevices = mediaDeviceInfoList => {
-  return filterInputMediaDevices(mediaDeviceInfoList).filter(device =>
-    device.kind.includes("audio")
-  );
-};
-
-/**
- * Returns a filtered array of MediaDeviceInfo[-like] structures, representing
- * video input devices only.
- *
- * NOTE: This was implemented as a separate array filter function in order to
- * perform unit testing.
- *
- * @param {MediaDeviceInfo[] | Object[]} mediaDeviceInfoList
- * @return {MediaDeviceInfo | Object}
- */
-const filterVideoInputDevices = mediaDeviceInfoList => {
-  return filterInputMediaDevices(mediaDeviceInfoList).filter(device =>
-    device.kind.includes("video")
-  );
-};
-
-/**
- * Returns a filtered array of MediaDeviceInfo[-like] structures, representing
- * output devices only.
- *
- * NOTE: This was implemented as a separate array filter function in order to
- * perform unit testing.
- *
- * @param {MediaDeviceInfo[] | Object[]} mediaDeviceInfoList
- * @return {MediaDeviceInfo | Object}
- */
-const filterOutputMediaDevices = mediaDeviceInfoList => {
-  return mediaDeviceInfoList.filter(device => device.kind.includes("output"));
-};
-
-/**
- * Returns a filtered array of MediaDeviceInfo[-like] structures, representing
- * audio output devices only.
- *
- * NOTE: This was implemented as a separate array filter function in order to
- * perform unit testing.
- *
- * @param {MediaDeviceInfo[] | Object[]} mediaDeviceInfoList
- * @return {MediaDeviceInfo | Object}
- */
-const filterAudioOutputDevices = mediaDeviceInfoList => {
-  return filterOutputMediaDevices(mediaDeviceInfoList).filter(device =>
-    device.kind.includes("audio")
-  );
-};
-
-/**
- * Returns a filtered array of MediaDeviceInfo[-like] structures, representing
- * video output devices only.
- *
- * NOTE: This was implemented as a separate array filter function in order to
- * perform unit testing.
- *
- * @param {MediaDeviceInfo[] | Object[]} mediaDeviceInfoList
- * @return {MediaDeviceInfo | Object}
- */
-const filterVideoOutputDevices = mediaDeviceInfoList => {
-  return filterOutputMediaDevices(mediaDeviceInfoList).filter(device =>
-    device.kind.includes("video")
-  );
-};
-
 module.exports = fetchMediaDevices;
 module.exports.fetchInputMediaDevices = fetchInputMediaDevices;
 
 module.exports.cacheDiffMediaDevices = cacheDiffMediaDevices;
-
-module.exports.filterInputMediaDevices = filterInputMediaDevices;
-module.exports.filterAudioInputDevices = filterAudioInputDevices;
-module.exports.filterVideoInputDevices = filterVideoInputDevices;
-
-module.exports.filterOutputMediaDevices = filterOutputMediaDevices;
-module.exports.filterAudioOutputDevices = filterAudioOutputDevices;
-module.exports.filterVideoOutputDevices = filterVideoOutputDevices;
