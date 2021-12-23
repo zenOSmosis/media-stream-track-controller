770d46b66e92a93f971beba5ff9cd4ae27bb2ed5
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-12-23 14:26:27 -0600

    Extract existing audioContext and mediaDevice tests

---
+++ test/mock-media-devices.js
@@ -0,0 +1,59 @@
+const MOCK_MEDIA_DEVICES = [
+  {
+    deviceId: "default",
+    kind: "audioinput",
+    label: "Default",
+    groupId: "356c14c86433feeb9a7af5d05949fdce43fe653d1b8a3c7c4700c05789961e51",
+  },
+  {
+    deviceId:
+      "d52f39bf56b99e78edfa08792464b41b253778c54101d1d6186cc2a3df1c5341",
+    kind: "audioinput",
+    label: "Built-in Audio Analog Stereo",
+    groupId: "665d59d67c28bfec8594f022af4246cb2db1fac5f8b20775932af647b18e2100",
+  },
+  {
+    deviceId:
+      "6d6687fed2540849d566df07c9d9a411fa6d54843a9c4028c247dba173c49800",
+    kind: "videoinput",
+    label: "HP HD Camera (05c8:0383)",
+    groupId: "19a792eb3fde82b819db42ff5d9c775d87c98b08852c276e40543ab36d4682df",
+  },
+  {
+    deviceId:
+      "e522c46bfc297e97a63abdd019ed73b7a48440d2e90f732ecea576d492b67693",
+    kind: "audioinput",
+    label: "Scarlett Solo USB Analog Stereo",
+    groupId: "9253a523d57bf06af9ce171e7ddc6befc8e4c0216f1eb8ace9d16beef14612dc",
+  },
+  {
+    deviceId: "default",
+    kind: "audiooutput",
+    label: "Default",
+    groupId: "default",
+  },
+  {
+    deviceId:
+      "4c71bcbab3d150ab5139c170c90bc79637fafd27a50e667085a8832262f1de71",
+    kind: "audiooutput",
+    label: "Built-in Audio Analog Stereo",
+    groupId: "665d59d67c28bfec8594f022af4246cb2db1fac5f8b20775932af647b18e2100",
+  },
+
+  {
+    deviceId:
+      "5qpbnstoq81w4oo1mas66pojuvqbx6hyrm82s7quzfo06bv8rhfzncdxruzn4ujm",
+    kind: "videooutput",
+    label: "Fake Video Output 1",
+    groupId: "kxwsy40vt6za71qzpcml5ygj4iz90el5utgezcqzpcuvzcypm577b61ouoz9ye5w",
+  },
+  {
+    deviceId:
+      "d073lm4q8rm6f8xako3d578y1wv8fuhw8stmiltu25p2ruxg2t460ex7fblc1gyh",
+    kind: "videooutput",
+    label: "Fake Video Output 2",
+    groupId: "o78uo5gwy86ipxzkm15atfc3ii9kqtm7aj5n9zdan8tpomhe3jswvpsl8541lic8",
+  },
+];
+
+module.exports = MOCK_MEDIA_DEVICES;
---
+++ test/utils.mediaDevice.test.js
@@ -0,0 +1,347 @@
+const test = require("tape");
+const { utils } = require("../src");
+const MOCK_MEDIA_DEVICES = require("./mock-media-devices");
+
+test("utils.mediaDevice.fetchMediaDevices", async t => {
+  const runOne = await utils.mediaDevice.fetchMediaDevices(false);
+  const runTwo = await utils.mediaDevice.fetchMediaDevices(false);
+
+  // Dynamically plan the test based on number of retrieved devices, which can
+  // vary across test runners, as they are browser-based
+  t.plan(runOne.length + 1);
+
+  t.ok(
+    typeof utils.mediaDevice.fetchMediaDevices(false).then === "function",
+    "fetchMediaDevices is a promise"
+  );
+
+  for (const idx in runOne) {
+    t.ok(
+      Object.is(runOne[idx], runTwo[idx]),
+      `mediaDeviceInfo element ${idx} is the same across two calls`
+    );
+  }
+
+  t.end();
+});
+
+test("utils.mediaDevice.cacheDiffMediaDevices", t => {
+  // FIXME: For this test, the t.plan() is not currently used because of the
+  // complexity in calculating it due to the way the test is constructed.
+  // Perhaps a better idea is to split this test up into smaller tests.
+
+  const PARALLEL_MOCK_MEDIA_DEVICES = [...MOCK_MEDIA_DEVICES].map(object => {
+    const ret = {};
+
+    Object.entries(object).forEach(([key, value]) => {
+      ret[key] = value;
+    });
+
+    return ret;
+  });
+
+  // Prelude test (doesn't directly test cacheDiffMediaDevices itself but
+  // ensures the passed in data is different, just as subsequent calls to the
+  // browser's fetchMediaDevices returns different instances)
+  for (const idx in MOCK_MEDIA_DEVICES) {
+    t.ok(
+      !Object.is(MOCK_MEDIA_DEVICES[idx], PARALLEL_MOCK_MEDIA_DEVICES[idx]),
+      `PARALLEL_MOCK_MEDIA_DEVICE idx ${idx} is not the same object instance as the original`
+    );
+  }
+
+  // Test w/ equal number of elements
+  (() => {
+    const cacheDiff = utils.mediaDevice.cacheDiffMediaDevices(
+      PARALLEL_MOCK_MEDIA_DEVICES,
+      MOCK_MEDIA_DEVICES
+    );
+
+    for (const idx in cacheDiff) {
+      t.ok(
+        Object.is(cacheDiff[idx], PARALLEL_MOCK_MEDIA_DEVICES[idx]),
+        `cacheDiff1 idx ${idx} is same object as parallel media devices with same idx`
+      );
+      t.ok(
+        !Object.is(cacheDiff[idx], MOCK_MEDIA_DEVICES[idx]),
+        `cacheDiff1 idx ${idx} is different object as mock media devices with same idx`
+      );
+    }
+  })();
+
+  // Test w/ removed elements
+  const cachedDiffRemoved = (() => {
+    const cachedDiffRemoved = utils.mediaDevice.cacheDiffMediaDevices(
+      PARALLEL_MOCK_MEDIA_DEVICES,
+      MOCK_MEDIA_DEVICES.slice(0, 4)
+    );
+
+    t.equals(cachedDiffRemoved.length, 4);
+
+    return cachedDiffRemoved;
+  })();
+
+  // Test w/ added elements
+  (() => {
+    t.ok(
+      cachedDiffRemoved.length < MOCK_MEDIA_DEVICES.length,
+      "removed cache diff has less elements than before cache re-add"
+    );
+
+    const updatedCacheDiff = utils.mediaDevice.cacheDiffMediaDevices(
+      cachedDiffRemoved,
+      MOCK_MEDIA_DEVICES
+    );
+
+    t.ok(
+      updatedCacheDiff.length > cachedDiffRemoved.length,
+      "updated add cache has more elements than removed cache"
+    );
+
+    t.equals(
+      updatedCacheDiff.length,
+      MOCK_MEDIA_DEVICES.length,
+      "updated add cache has same amount of elements as expected"
+    );
+  })();
+
+  t.end();
+});
+
+test("utils.mediaDevice.mediaDeviceInfoFilters", t => {
+  t.plan(6);
+
+  t.deepEquals(
+    utils.mediaDevice.mediaDeviceInfoFilters.filterInputMediaDevices(
+      MOCK_MEDIA_DEVICES
+    ),
+    MOCK_MEDIA_DEVICES.filter(device => device.kind.includes("input")),
+    "deep equals input media devices"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.mediaDeviceInfoFilters.filterAudioInputDevices(
+      MOCK_MEDIA_DEVICES
+    ),
+    MOCK_MEDIA_DEVICES.filter(device => device.kind.includes("audioinput")),
+    "deep equals audio input devices"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.mediaDeviceInfoFilters.filterVideoInputDevices(
+      MOCK_MEDIA_DEVICES
+    ),
+    MOCK_MEDIA_DEVICES.filter(device => device.kind.includes("videoinput")),
+    "deep equals video input devices"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.mediaDeviceInfoFilters.filterOutputMediaDevices(
+      MOCK_MEDIA_DEVICES
+    ),
+    MOCK_MEDIA_DEVICES.filter(device => device.kind.includes("output")),
+    "deep equals output media devices"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.mediaDeviceInfoFilters.filterAudioOutputDevices(
+      MOCK_MEDIA_DEVICES
+    ),
+    MOCK_MEDIA_DEVICES.filter(device => device.kind.includes("audiooutput")),
+    "deep equals audio output devices"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.mediaDeviceInfoFilters.filterVideoOutputDevices(
+      MOCK_MEDIA_DEVICES
+    ),
+    MOCK_MEDIA_DEVICES.filter(device => device.kind.includes("videooutput")),
+    "deep equals video output devices"
+  );
+
+  t.end();
+});
+
+test("utils.mediaDevice.getMatchedMediaDevice", t => {
+  t.plan(8);
+
+  t.throws(
+    () => {
+      utils.mediaDevice.getMatchedMediaDevice(
+        {
+          deviceId: "old-device-id",
+          kind: "audioinput",
+          label: "Built-in Audio Analog Stereo",
+          groupId: "old-group-id",
+        },
+        MOCK_MEDIA_DEVICES
+      );
+    },
+    ReferenceError,
+    "throws ReferenceError when no kind is specified"
+  );
+
+  t.throws(
+    () => {
+      utils.mediaDevice.getMatchedMediaDevice(
+        "fake-kind",
+        {
+          deviceId: "old-device-id",
+          kind: "audioinput",
+          label: "Built-in Audio Analog Stereo",
+          groupId: "old-group-id",
+        },
+        MOCK_MEDIA_DEVICES
+      );
+    },
+    ReferenceError,
+    "throws ReferenceError when invalid kind is specified"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.getMatchedMediaDevice(
+      "audioinput",
+      {
+        deviceId: "old-device-id",
+        kind: "audioinput",
+        label: "Built-in Audio Analog Stereo",
+        groupId: "old-group-id",
+      },
+      MOCK_MEDIA_DEVICES
+    ),
+    {
+      deviceId:
+        "d52f39bf56b99e78edfa08792464b41b253778c54101d1d6186cc2a3df1c5341",
+      kind: "audioinput",
+      label: "Built-in Audio Analog Stereo",
+      groupId:
+        "665d59d67c28bfec8594f022af4246cb2db1fac5f8b20775932af647b18e2100",
+    },
+    "matches on changed device and group id"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.getMatchedMediaDevice(
+      "audioinput",
+      {
+        deviceId:
+          "d52f39bf56b99e78edfa08792464b41b253778c54101d1d6186cc2a3df1c5341",
+      },
+      MOCK_MEDIA_DEVICES
+    ),
+    {
+      deviceId:
+        "d52f39bf56b99e78edfa08792464b41b253778c54101d1d6186cc2a3df1c5341",
+      kind: "audioinput",
+      label: "Built-in Audio Analog Stereo",
+      groupId:
+        "665d59d67c28bfec8594f022af4246cb2db1fac5f8b20775932af647b18e2100",
+    },
+    "matches on device id"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.getMatchedMediaDevice(
+      "audioinput",
+      {
+        label: "Scarlett Solo USB Analog Stereo",
+      },
+      MOCK_MEDIA_DEVICES
+    ),
+    {
+      deviceId:
+        "e522c46bfc297e97a63abdd019ed73b7a48440d2e90f732ecea576d492b67693",
+      kind: "audioinput",
+      label: "Scarlett Solo USB Analog Stereo",
+      groupId:
+        "9253a523d57bf06af9ce171e7ddc6befc8e4c0216f1eb8ace9d16beef14612dc",
+    },
+    "matches on label"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.getMatchedMediaDevice(
+      "audioinput",
+      {
+        label: undefined,
+      },
+      MOCK_MEDIA_DEVICES
+    ),
+    null,
+    "no match on undefined label"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.getMatchedMediaDevice(
+      "audioinput",
+      {
+        label: "some-unknown-label",
+      },
+      MOCK_MEDIA_DEVICES
+    ),
+    null,
+    "no match on unknown label"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.getMatchedMediaDevice(
+      "audioinput",
+      {},
+      MOCK_MEDIA_DEVICES
+    ),
+    null,
+    "no match on no previous info"
+  );
+
+  t.end();
+});
+
+test("utils.mediaDevice.getMediaDeviceTrackControllers", t => {
+  t.plan(2);
+
+  t.throws(
+    () => {
+      utils.mediaDevice.getMediaDeviceTrackControllers({ label: test });
+    },
+    ReferenceError,
+    "throws ReferenceError when no deviceId is set"
+  );
+
+  t.deepEquals(
+    utils.mediaDevice.getMediaDeviceTrackControllers({
+      deviceId: "test",
+    }),
+    [],
+    "matches empty array for controllers with device id of test"
+  );
+
+  // NOTE: At this time, it may be nearly impossible to automatically test this
+  // function completely works, so refer to dev UI for manual testing
+
+  t.end();
+});
+
+test("utils.mediaDevice.getIsMediaDeviceBeingCaptured", t => {
+  t.plan(2);
+
+  t.throws(
+    () => {
+      utils.mediaDevice.getIsMediaDeviceBeingCaptured({ label: test });
+    },
+    ReferenceError,
+    "throws ReferenceError when no deviceId is set"
+  );
+
+  t.equals(
+    utils.mediaDevice.getIsMediaDeviceBeingCaptured({
+      deviceId: "test",
+    }),
+    false,
+    "test device id is not reported as being captured"
+  );
+
+  // NOTE: At this time, it may be nearly impossible to automatically test this
+  // function completely works, so refer to dev UI for manual testing
+
+  t.end();
+});
