d7c40dc59150b5cfda19a92eeb3fdf320e250800
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-12-26 18:24:50 -0600

    Relax rules which previously would have made getIsSameMediaDevice throw errors, now returning false

--- src/utils/mediaDevice/getIsSameMediaDevice.js
+++ src/utils/mediaDevice/getIsSameMediaDevice.js
@@ -1,10 +1,13 @@
 const { MEDIA_DEVICE_KINDS } = require("../../constants");
+const { logger } = require("phantom-core");
 
 /**
  * Determines whether the given media devices are the same.
  *
  * NOTE: One of these devices may represent partial data, such as one of them
- * only having a deviceId.
+ * only having a deviceId. It is in hopes that the object would not be run
+ * through this comparison algorithm if the type was never known, however, as a
+ * fallback, the enforcedKind will add additional checking.
  *
  * @param {MediaDeviceInfo | Object} deviceA Partial, or complete data
  * representation of the device
@@ -23,29 +26,35 @@ module.exports = function getIsSameMediaDevice(
     throw new ReferenceError(`Invalid enforcedKind "${enforcedKind}`);
   }
 
+  if (typeof deviceA !== "object" || typeof deviceB !== "object") {
+    throw new TypeError("deviceA and deviceB must be objects");
+  }
+
+  // Shallow copy devices so we can alter their properties, if need-be
   const locDeviceA = { ...deviceA };
   const locDeviceB = { ...deviceB };
 
   if (enforcedKind) {
+    // If neither device has a kind associated to it, return false
     if (!locDeviceA.kind && !locDeviceB.kind) {
-      throw new ReferenceError(
-        "At least one of the compared devices must have a reference kind"
-      );
+      return false;
     }
 
+    // Patch deviceA kind if previously non-existent, patch it with the
+    // enforcedKind
     if (!locDeviceA.kind) {
       locDeviceA.kind = enforcedKind;
     }
 
+    // Patch deviceB kind if previously non-existent, patch it with the
+    // enforcedKind
     if (!locDeviceB.kind) {
       locDeviceB.kind = enforcedKind;
     }
   }
 
   if (!locDeviceA.kind || !locDeviceB.kind) {
-    throw new ReferenceError(
-      "partialKind must be specified if there are no available kinds"
-    );
+    return false;
   } else if (!MEDIA_DEVICE_KINDS.includes(locDeviceA.kind)) {
     throw new ReferenceError(`Invalid kind for deviceA "${locDeviceA.kind}"`);
   } else if (!MEDIA_DEVICE_KINDS.includes(locDeviceB.kind)) {
