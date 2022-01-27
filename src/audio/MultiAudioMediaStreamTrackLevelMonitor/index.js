0309212869ee05e2fcad35585acdaadabbde2b9c
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-01-27 13:36:27 -0600

    Remove non-utilized event

--- src/audio/MultiAudioMediaStreamTrackLevelMonitor/index.js
+++ src/audio/MultiAudioMediaStreamTrackLevelMonitor/index.js
@@ -1,15 +1,9 @@
 const MultiAudioMediaStreamTrackLevelMonitor = require("./MultiAudioMediaStreamTrackLevelMonitor");
-const {
-  EVT_AUDIO_LEVEL_UPDATED,
-  EVT_DEBOUNCED_PEAK_AUDIO_LEVEL_TICK,
-  EVT_AUDIO_ERROR,
-  EVT_AUDIO_ERROR_RECOVERED,
-} = MultiAudioMediaStreamTrackLevelMonitor;
+const { EVT_AUDIO_LEVEL_UPDATED, EVT_AUDIO_ERROR, EVT_AUDIO_ERROR_RECOVERED } =
+  MultiAudioMediaStreamTrackLevelMonitor;
 
 module.exports = MultiAudioMediaStreamTrackLevelMonitor;
 
 module.exports.EVT_AUDIO_LEVEL_UPDATED = EVT_AUDIO_LEVEL_UPDATED;
-module.exports.EVT_DEBOUNCED_PEAK_AUDIO_LEVEL_TICK =
-  EVT_DEBOUNCED_PEAK_AUDIO_LEVEL_TICK;
 module.exports.EVT_AUDIO_ERROR = EVT_AUDIO_ERROR;
 module.exports.EVT_AUDIO_ERROR_RECOVERED = EVT_AUDIO_ERROR_RECOVERED;
